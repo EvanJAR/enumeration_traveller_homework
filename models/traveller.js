e2ac3efd04caff7b6491aecf0fdf43b407cdb9cc
Evan Reid <evan_1995@hotmail.com>
2020-12-09 21:39:33 +0000

    some psuedocode for number one

--- models/traveller.js
+++ models/traveller.js
@@ -3,6 +3,11 @@ const Traveller = function(journeys) {
 };
 
 Traveller.prototype.getJourneyStartLocations = function() {
+  //GOAL: I want to return an array of all the startLocations
+  // create a new variable x
+  // the value of the variable will be an arrow function that uses a map method that creates an array of the same length as "journeys" 
+  // return "journey.startLocation"
+  // return variable x outside of the loop in order to return the desired array.
 
 };
 
