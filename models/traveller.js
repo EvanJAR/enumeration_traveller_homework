9065136fe74b331f247054c6f1d7326ff1426408
Evan Reid <evan_1995@hotmail.com>
2020-12-09 21:51:43 +0000

    getJourneyEndLocations passing test w/ psuedocode (henceforth called PC)

--- models/traveller.js
+++ models/traveller.js
@@ -10,7 +10,16 @@ Traveller.prototype.getJourneyStartLocations = function() {
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
-
+  // GOAL: return an array of endLocations
+  // use a method that will create an array of the same length and return only the endLocations of each 'journey' object in the array
+  // the map method, as in the first example, will serve this purpose
+  // create a variable called journeyEndLocations
+  // use map method on journeys and return journey.endLocation
+  const journeyEndLocations = this.journeys.map((journey) => {
+    return journey.endLocation;
+  });
+  // return journeyEndLocations outside the loop to return new array
+  return journeyEndLocations;
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
