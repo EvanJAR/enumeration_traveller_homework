9a12956923ab55cd3d2dbc9f56492c156c655afe
Evan Reid <evan_1995@hotmail.com>
2020-12-09 21:45:00 +0000

    first method passing w/ pseudocode

--- models/traveller.js
+++ models/traveller.js
@@ -4,11 +4,14 @@ const Traveller = function(journeys) {
 
 Traveller.prototype.getJourneyStartLocations = function() {
   //GOAL: I want to return an array of all the startLocations
-  // create a new variable x
-  // the value of the variable will be an arrow function that uses a map method that creates an array of the same length as "journeys" 
-  // return "journey.startLocation"
-  // return variable x outside of the loop in order to return the desired array.
-
+  // create new variable journeyStartLocations
+  // value of the variable an arrow function that applies map method on array 
+    const journeyStartLocations = this.journeys.map((journey) => {
+  // return "journey.startLocation"      
+      return journey.startLocation; 
+    });
+  // return variable journeyStartLocations outside of the loop in order to return the desired array.
+    return journeyStartLocations;
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
