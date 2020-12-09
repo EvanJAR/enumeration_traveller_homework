e5accc7a31ad7f4f0e50a41170feaee29ec51fc9
Evan Reid <evan_1995@hotmail.com>
2020-12-09 22:02:47 +0000

    getJourneysByTransport passes w/o PC

--- models/traveller.js
+++ models/traveller.js
@@ -17,12 +17,7 @@ Traveller.prototype.getJourneyEndLocations = function () {
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
-  // GOAL: return an array of journeys that have a particular transport
-  // I think that a filter method might be the best option here, find method will only return the FIRST value which meets the desired criteria, where filter will return a new array with multiple elements
-  // create a new variable journeysByTransport 
-  // value of this variable will be applying the filter method on journeys and will return an array of journeys if journey.transport === transport (that is being searched for)
   const journeysByTransport = this.journeys.filter(journey => journey.transport === transport);
-  // return journeysByTransport outside of loop
   return journeysByTransport;
 };
 
